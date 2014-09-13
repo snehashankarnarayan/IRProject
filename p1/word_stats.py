953f8ea345f1eff8497f270dd3a6128a7d6b209a
snehashankarnarayan <snehas@cs.umass.edu>
2014-09-13 19:00:15 -0400

    some skeleton of p1

--- p1/word_stats.py
+++ p1/word_stats.py
@@ -5,6 +5,9 @@ from os import listdir
 import re
 import sys
 from time import time
+import string
+from collections import Counter
+from sets import Set
 
 #Constants
 tinyDirName = "/phoenix/ir_code/data/books-tiny/"
@@ -13,13 +16,18 @@ smallDirName = "/phoenix/ir_code/data/books-small/"
 bigDirName = "/phoenix/ir_code/data/books-big/"
 
 #Globals
-global wordCount;
+wordCount = 0
+pageCount = 0
+pageText = ""
 
-def countString(text):
-    global wordCount
-    words = re.split(" ", text)
-    wordCount += len(words)
-    return len(words)
+#Global hashtables/dictionaries
+globalWordHash = Counter()
+bookWordHash = Counter()
+pageWordHash = Counter()
+
+#Per book sets
+bookWordSet = Set()
+
 def getFileList(rootDirName):
     tinyDir =  listdir(rootDirName);
     fileList = []
@@ -29,14 +37,60 @@ def getFileList(rootDirName):
             fileList.append(rootDirName + dirName + "/" + xmlFile)
     return fileList
 
+def processPage():
+    global pageCount
+    global globalWordHash
+    global pageText
+    global bookWordSet
+    pageCount = pageCount + 1
+    
+    #Text processing in page
+    words = re.split(" ", pageText)
+    for( i = 0; i < len(words); i++):
+        words[i] = words[i].translate(None, string.punctuation)
+
+        if(globalWordHash.has_key(word)):
+            globalWordHash[word] = globalWordHash[word] + 1
+        else:
+            globalWordHash[word] = 1
+    
+    #Page specific processing
+    wordSet = set(words)
+    for word in wordSet:
+        word = word.translate(None, string.punctuation)
+        if(pageWordHash.has_key(word)):
+            pageWordHash[word] = pageWordHash[word] + 1
+        else:
+            pageWordHash[word] = 1
+    
+    bookWordSet.update(wordSet)
+    #sys.exit(0)
+
+
+
 def processFile(fileName):
+    global pageText
+    global bookWordHash
+    global bookWordSet
+    bookWordSet.clear()
     for event, elem in ET.iterparse(fileName):
-        if event == 'end':
-            if(elem.text != None):
-                countString(elem.text)
+        if (elem.tag == "page" and event == 'start'):
+            pageText = " "
+        if(elem.tag == "line" and event == 'end' and elem.text != None):
+            pageText = pageText + " " + elem.text
+        if(elem.tag == "page" and event == 'end'):
+            processPage()
         elem.clear() 
 
-def word_stats(dirName):
+    #Update book hash
+    for word in bookWordSet:
+        if(bookWordHash.has_key(word)):
+            bookWordHash[word] = bookWordHash[word] + 1
+        else:
+            bookWordHash[word] = 1
+    
+
+def processDirectory(dirName):
     fileList = getFileList(dirName)
     for fileName in fileList:
         try:
@@ -51,16 +105,29 @@ def word_stats(dirName):
             print "Unexpected Error.. Continue anyway"
             pass
 
-if __name__ == "__main__":
+def testMe():
+    fileList = getFileList(tinyDirName)
+    processFile(fileList[0])
+    print "WordCount " + str(wordCount)
+    print "PageCount " + str(pageCount)
+    print "GlobalWordHash"
+    print globalWordHash
+    print "pageWordHash"
+    print pageWordHash
+    print "BookWordHash"
+    print bookWordHash
 
+if __name__ == "__main__":
     t1 = time()
-    if sys.argv[1] == "tiny":
-        word_stats(tinyDirName)
+    if sys.argv[1] == "test":
+        testMe()
+    elif sys.argv[1] == "tiny":
+        processDirectory(tinyDirName)
     elif sys.argv[1] == "small":
-        word_stats(smallDirName)
+        processDirectory(smallDirName)
     elif sys.argv[1] == "medium":
-        word_stats(mediumDirName)
+        processDirectory(mediumDirName)
     elif sys.argv[1] == "big":
-        word_stats(bigDirName)
+        processDirectory(bigDirName)
     t2 = time()
     print 'Time taken in seconds: %f' %(t2-t1)
