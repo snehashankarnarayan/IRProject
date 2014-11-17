18c0b238c9c3a0c8151aaa02af4f5c7ffd9517ba
snehashankarnarayan <snehas@cs.umass.edu>
2014-11-17 01:00:39 -0500

    process_data update

--- p2/step5/process_data.py
+++ p2/step5/process_data.py
@@ -24,6 +24,7 @@ def calculate_measures(filename, rankfilename, rang):
             relList = dict()
         currentQuery = words[0]
         relList[words[1]] = int(words[2].strip())
+    queryList[currentQuery] = dict(relList)
     fp.close()
 
     #Processing ranked list
@@ -40,32 +41,51 @@ def calculate_measures(filename, rankfilename, rang):
             relList = list()
         currentQuery = words[0]
         relList.append(words[2])
+    entity = judgements(currentQuery, list(relList))
+    rankList.append(entity)
     rfile.close()
     
     #Compute mean avg precision
     mean_avg = 0.0
+    qCounter = 0
     for item in rankList:
-        recall = 0
+        counter = 0
         avg_prec = 0.0
         prec10 = 0.0
-        prec10_counter = 1
+        prec = 0.0
+        recall = 0
         qEntity = queryList.get(item.query)
         if(qEntity != None):
+            qCounter += 1
             for docID in item.rel:
                 relevance = qEntity.get(docID, 0)
+                #if(item.query == '17'):
+                 #   print relevance
+                counter += 1
                 if(relevance >= int(rang)):
-                    avg_prec += 1
+                    prec += 1.0 
                     recall += 1
-                    if(prec10_counter <= 10):
-                        prec10 += 1
-                prec10_counter += 1
-            if(recall != 0):
-                avg_prec = avg_prec/recall
+                    avg_prec += (prec/counter)
+                    if(counter <= 10):
+                        prec10 += 1.0
+            
+            #get all relevant documents
+            rel_doc_num = 0
+            rel_docs = qEntity.values()
+            for val in rel_docs:
+                if(val >= int(rang)):
+                    rel_doc_num += 1
+            if(rel_doc_num != 0):
+                if(rel_doc_num > 100):
+                    rel_doc_num = 100
+                avg_prec = avg_prec/rel_doc_num
                 outfile.write("Avg precision " + item.query + " " + str(avg_prec) + "\n")
                 mean_avg += avg_prec
+            else:
+                outfile.write("Avg precision " + item.query + " No relevant docs" + "\n")
             prec10 = prec10/10
             outfile.write("Precision at 10: " + item.query + " " + str(prec10) + "\n")
-    mean_avg = mean_avg/len(rankList)
+    mean_avg = mean_avg/qCounter
     outfile.write("Mean Avg precision " + str(mean_avg) + "\n")
 
     for item in rankList:
@@ -80,7 +100,7 @@ def calculate_measures(filename, rankfilename, rang):
                 if(counter == 20):
                     break
             sList = list(rList)
-            sList.sort()
+            sList.sort(reverse = True)
             dcg = 0.0
             idcg = 0.0
             for i in range(0, len(rList)):
