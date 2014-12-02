7f338fc07f32423840e82f330394a924c90107bb
snehashankarnarayan <snehas@cs.umass.edu>
2014-12-02 12:50:16 -0500

    removing some commit

--- p3/correlation-thesaurus/classes.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-class judgements:
-    def __init__(self, query, rel):
-        #Globals
-        self.query = query
-        self.rel = rel
--- p3/correlation-thesaurus/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-./process_data.py books.qrels books-ranked-list.txt 2
-./process_data.py robust.qrels robust-class-ranked-list.txt 2
-./process_data.py robust04.qrels robust-queries-ranked-list.txt 1
